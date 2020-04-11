678180d1f8206fa08a29e9f42a6791dcb7c28750
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-11 20:21:14 +0530

    Added Flat Buttons

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -11,6 +11,41 @@ class HomePage extends StatelessWidget {
           child: Center(
             child: Column(
               children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*0.46,
+                        height: MediaQuery.of(context).size.height*0.23,
+                        margin: EdgeInsets.only(top: 8),
+                        child: FlatButton(
+                          child: null,
+                          color: Colors.green,
+                          onPressed: () {
+                            final player = AudioCache();
+                            player.play('note1.wav');
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*0.46,
+                        height: MediaQuery.of(context).size.height*0.23,
+                        margin: EdgeInsets.only(top: 8),
+                        child: FlatButton(
+                          child: null,
+                          color: Colors.blue,
+                          onPressed: () {
+                            final player = AudioCache();
+                            player.play('note2.wav');
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
 
               ],
             ),
